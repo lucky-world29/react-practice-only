39f01991257887be15191e60cf01a14231cdc1d4
Abinash <abinashbehera9898@gmail.com>
2024-09-20 18:54:06 +0530

    2nd react project

---
+++ Programming-with-mosh/src/Message.tsx
@@ -0,0 +1,19 @@
+// Create A react component - 1.javascript class
+//                          - 2. function **
+
+
+function Message(){
+    // JSX : JavaScript XML
+        // easily create Dynamic Content .
+    const name = "Lucky";
+    return <>
+    <h1><b>{name}</b></h1>
+    <h1>Hello World !</h1>
+    </>;
+    
+    //return <h1>Hello World !</h1>;
+    
+    
+}
+
+export default Message;
