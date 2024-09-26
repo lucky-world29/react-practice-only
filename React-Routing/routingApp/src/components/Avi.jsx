0781f2f96c8b26c6ad4b8d94d684cc179e707c6e
ABINASH BEHERA <128899808+lucky-1-4-3@users.noreply.github.com>
2024-09-26 12:33:47 +0530

    .

--- React-Routing/routingApp/src/components/Avi.jsx
+++ React-Routing/routingApp/src/components/Avi.jsx
@@ -1,15 +1,20 @@
 import React from "react";
 
-const Avi = () =>{
+const Avi = () => {
     return (
     <>
         <div>
-            <h1>Hello Lucky</h1>
-            <p>My name is Avi</p>
-            
+        <h1>Hello Lucky</h1>
+        <p>My name is Avi</p>
+        <pre>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odio neque
+                    <br />voluptate, officia repellat earum, deserunt modi illo optio reiciendis
+                    <br />facilis quaerat nisi totam quo quae perferendis molestias voluptatem?
+                <br />    Consectetur, dolores!
+        </pre>
         </div>
     </>
-    )
-}
+    );
+};
 
 export default Avi;
